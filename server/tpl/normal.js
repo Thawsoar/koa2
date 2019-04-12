36c14655d379273cd01c70db4c0b17e61d6250de
thawsoar <1306750238@qq.com>
2019-04-12 15:21:27 +0800

    no message

---
+++ server/tpl/normal.js
@@ -0,0 +1,18 @@
+module.exports = `
+	<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1"
+			<title>Document</title>
+		</head>
+		<body>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8">
+						<h1>我是主页</h1>
+					</div>
+				</div>
+		</body>
+	</html>
+`
